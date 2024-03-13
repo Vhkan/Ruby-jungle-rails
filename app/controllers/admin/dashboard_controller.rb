162d9d6a7f88dac6ffb0ad7e82d5e563618f5d78
Vhkan <vhkan@yahoo.com>
2024-03-12 20:39:05 -0600

    Add admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,18 @@
 class Admin::DashboardController < ApplicationController
+
+  before_action :authenticate
+
   def show
     @product_total = Product.count
     @category_total = Category.count
+  end
+
+  private
+
+  #authenticate method
+  def authenticate
+    authenticate_or_request_with_http_basic("Administration") do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
